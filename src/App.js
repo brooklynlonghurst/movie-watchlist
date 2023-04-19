8887fc3ae334f8284bb09ec385f2d9fc66b4edcd
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-04-18 18:23:01 -0600

    working on useEffect hook

--- src/App.js
+++ src/App.js
@@ -3,21 +3,36 @@ import './App.css';
 import axios from "axios"
 import Header from './components/Header'
 import MovieScreen from "./components/MovieScreen";
+import WatchList from "./components/WatchList";
+
 
 function App() {
   const [list, setList] = useState([])
   const [movieList, setMovieList] = useState([])
   const [page, setPage] = useState(1)
 
-  function getData() {
-    axios
-      .get(`https://api.themoviedb.org/3/movie/550?api_key=${process.env.REACT_APP_API_KEY}&language=en=US&page=${page}`)
-      .then((res) => {
-        console.log(res.data.results)
-        setMovieList(res.data.results)
-      })
+  function addMovie(movie) {
+    setList([...list, movie])
   }
 
+  function removeMovie(movie) {
+    const newState = list.filter((mov) => {
+      return (mov !== movie)
+    })
+    setList(newState)
+  }
+
+  function getData() {
+    axios
+    .get(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`
+    )
+    .then((res) => {
+      console.log(res.data.results);
+      setMovieList(res.data.results);
+    });
+  }
+  
   useEffect(() => {
     getData()
   }, [page])
@@ -27,10 +42,15 @@ function App() {
       <Header />
       <main>
       <MovieScreen 
+        addMovie={addMovie}
         movieList={movieList}
         page={page}
         setPage={setPage}
         list={list}
+        removeMovie={removeMovie}
+      />
+      <WatchList
+      list={list}
       />
       </main>
     </div>
--- src/components/MovieScreen.jsx
+++ src/components/MovieScreen.jsx
@@ -1,15 +1,36 @@
 import React from "react"
+import MovieCard from "./MovieCard"
 
-function MovieScreen(props) {
+function MovieScreen({addMovie, movieList, removeMovie, list, page, setPage}) {
     const movieDisplay = movieList.map((movie, index) => {
-        return <h2>{movie.orginal_title}</h2>
+        return (
+            <MovieCard 
+                addMovie={addMovie} 
+                movie={movie} 
+                list={list} 
+                removeMovie={removeMovie}
+            />
+        )
     })
+
+    function decrement() {
+        setPage(page - 1)
+    }
+
+    function increment() {
+        setPage(page + 1)
+    }
 
     return (
         <div className="page">
             <h1>Brooklyn's Movie Theatre</h1>
             <h3>Add a movie to your watchlist!</h3>
+            <div className="btn-container">
+                <button onClick={page !== 1 && decrement}>Previous</button>
+                <button onClick={increment}>Next</button>
+            </div>
             <div className="movie-container">{movieDisplay}</div>
+            
         </div>
     )
 }
